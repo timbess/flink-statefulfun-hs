b4430fd99b2446d1c5cb84a9cdc6f9b6679d5049
Timothy Bess <tdbgamer@gmail.com>
2020-09-17 12:09:03 -0400

    initial haskell.nix refactor

--- shell.nix
+++ shell.nix
@@ -1,16 +1,17 @@
-{ haskellNixSrc ? builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz
+{ nixpkgs ? import <nixpkgs> {}
+, haskellNixSrc ? builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz
 , haskellNix ? import haskellNixSrc {}
-, nixpkgs ? haskellNix.sources.nixpkgs }:
+}:
 
 let
-  pkgs = import nixpkgs haskellNix.nixpkgsArgs;
+  pkgs = import haskellNix.sources.nixpkgs haskellNix.nixpkgsArgs;
 in
   pkgs.mkShell {
     buildInputs = (with pkgs; [
       haskell.compiler.ghc883
       glibc glibc.static
       zlib zlib.static
-      gmp5 gmp5.static
+      gmp6 (gmp6.override { withStatic = true; })
       gcc
       protobuf
       wget
