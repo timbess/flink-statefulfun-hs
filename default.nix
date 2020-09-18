5553353a2387b06f0b377baf29d8e67195d75724
Timothy Bess <tdbgamer@gmail.com>
2020-09-17 23:32:20 -0400

    * working haskell.nix setup

--- default.nix
+++ default.nix
@@ -8,24 +8,39 @@
 
 # haskell.nix provides some arguments to be passed to nixpkgs, including some
 # patches and also the haskell.nix functionality itself as an overlay.
-, nixpkgsArgs ? ( haskellNix.nixpkgsArgs // {
-    overlays = haskellNix.nixpkgsArgs.overlays ++ [
-      (self: super: {
-        proto-lens-protoc = self.protobuf;
-      })
-    ];
-  })
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+# , nixpkgsArgs ? ( haskellNix.nixpkgsArgs // {
+#     overlays = haskellNix.nixpkgsArgs.overlays ++ [
+#       (self: super: {
+#         haskell.packages.ghc883.proto-lens-protobuf-types = self.haskell.packages.ghc883.proto-lens-protobuf-types.
+#       })
+#     ];
+#   })
 
 # import nixpkgs with overlays
 , pkgs ? import nixpkgsSrc nixpkgsArgs
-}: pkgs.haskell-nix.cabalProject {
+}: let
+
+package = pkgs.haskell-nix.cabalProject {
   # 'cleanGit' cleans a source directory based on the files known by git
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "flink-statefulfun";
     src = ./.;
   };
   
+  modules = [
+    {
+      packages.proto-lens-protobuf-types.components.library.build-tools = [pkgs.protobuf];
+    }
+  ];
 
   # For `cabal.project` based projects specify the GHC version to use.
   compiler-nix-name = "ghc883"; # Not used for `stack.yaml` based projects.
-}
+};
+
+new-proto-lens-protobuf-types = package.proto-lens-protobuf-types // {
+  setup = (package.proto-lens-protobuf-types.setup.overrideAttrs (old: { buildInputs = old.buildInputs ++ [pkgs.protobuf]; }));
+  src = (package.proto-lens-protobuf-types.src.overrideAttrs (old: { buildInputs = old.buildInputs ++ [pkgs.protobuf]; }));
+};
+
+in package # // { proto-lens-protobuf-types = new-proto-lens-protobuf-types; }
