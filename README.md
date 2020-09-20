038d6024f11e690f8c8a2649ebc1f1a437231918
Timothy Bess <tdbgamer@gmail.com>
2020-09-20 11:29:48 -0400

    Setup static compilation against `musl`

--- README.md
+++ README.md
@@ -2,12 +2,25 @@
 
 Provides a typed API for creating [flink stateful functions](https://flink.apache.org/news/2020/04/07/release-statefun-2.0.0.html). Greeter example is in [example/greeter/main/Main.hs](example/greeter/main/Main.hs)
 
-
 ## How to run example
 
 ```bash
-$ cd example
-$ docker-compose build
-$ docker-compose up -d
-$ docker-compose logs -f event-generator
+cd example
+docker-compose build
+docker-compose up -d
+docker-compose logs -f event-generator
+```
+
+## How to compile locally
+
+1. [Install nix](https://nixos.org/download.html)
+2. [Install cachix](https://github.com/cachix/cachix#installation)
+3. Setup nix cache 
+```bash
+cachix use iohk
+cachix use static-haskell-nix
+```
+4. Compile
+```bash
+cabal build
 ```
--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-{ nixpkgs ? import <nixpkgs> {}
+{ muslpkgs ? (import "${staticNix}/nixpkgs.nix").pkgsMusl
+, nixpkgs ? import <nixpkgs> {}
+, staticNix ? nixpkgs.fetchgit {
+  url = "https://github.com/nh2/static-haskell-nix";
+  sha256 = "0y97fvkjhvx17rmfdlymn4hwnq4r9nhf2r4m1s9fkh8gb8hfxp1s";
+  rev = "dbce18f4808d27f6a51ce31585078b49c86bd2b5";
+}
+# { nixpkgs ? import <nixpkgs> {}
 , haskellNixSrc ? builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz
 , haskellNix ? import haskellNixSrc {}
 }:
@@ -8,16 +15,14 @@ let
 in
   pkgs.mkShell {
     buildInputs = (with pkgs; [
-      haskell.compiler.ghc883
-      glibc glibc.static
-      zlib zlib.static
-      gmp6 (gmp6.override { withStatic = true; })
+      muslpkgs.haskell.compiler.ghc882
+      muslpkgs.zlib.static
+      (muslpkgs.gmp6.override { withStatic = true; })
       gcc
       protobuf
       wget
       curl
       git
-      libffi
       (libffi.overrideAttrs (old: { dontDisableStatic = true; }))
     ]) ++ (with pkgs.haskellPackages; [
       hlint
